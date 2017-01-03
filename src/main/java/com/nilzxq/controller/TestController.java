9dfd27ff1857fac55bddf6a5099d6469b837c854
nilzxq <2013358717@qq.com>
2017-01-03 20:37:06 +0800

    json

--- src/main/java/com/nilzxq/controller/TestController.java
+++ src/main/java/com/nilzxq/controller/TestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -99,5 +100,11 @@ public class TestController {
 	@ResponseBody
 	public String map(UserMapForm userMapForm){
 		return userMapForm.toString();	
+	}
+	
+	@RequestMapping(value="json.do")
+	@ResponseBody
+	public String json(@RequestBody User user){
+		return user.toString();	
 	}
 }
